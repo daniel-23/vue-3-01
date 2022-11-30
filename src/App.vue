0189180e25c3662e802988329fb31cacc1755200
Daniel Díaz <danielbatlet@gmail.com>
2022-11-30 10:34:44 -0500

    Commit inicial

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data: () => ({
+    contador: 0,
+  }),
+  methods: {
+    suma() {
+      this.contador++;
+    },
+    resta() {
+      if (this.contador > 0) {
+        this.contador--;
+      }
+    },
+    reset() {
+      this.contador = 0;
+    },
+  },
+};
+</script>
+
+<template>
+  <img src="./assets/logo.svg" alt="Logo" />
+  <h1>Hola mundo desde vue js 3</h1>
+  <hr />
+  <p>Contador: {{ contador }}</p>
+
+  <button v-on:click="suma">+1</button>
+  <button @click="resta">-1</button>
+  <button @click="reset">Reset</button>
+</template>
+
+<style scoped>
+h1 {
+  background-color: bisque;
+}
+</style>
